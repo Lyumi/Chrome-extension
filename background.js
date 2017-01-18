e799e752451a30822b2a4fddef09ce1f62aaec24
Jun <bjyoon513@gmail.com>
2017-01-18 12:24:03 -0500

    Get title of Website  Pleas enter the commit message for your changes. Lines starting

--- background.js
+++ background.js
@@ -1,1 +1,3 @@
-alert(document.location);
+chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
+  alert(response);
+});
---
+++ cite.js
@@ -0,0 +1,5 @@
+//chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerText);
+//chrome.runtime.sendMessage(document.querySelector('meta[property="og:title"]').getAttribute('content'));
+//var title = document.querySelector('meta[property="og:title"]').getAttribute('content');
+//var author = document.querySelector('meta[name="author]').getAttribute('content');
+chrome.runtime.sendMessage(document.querySelector('meta[property="og:title"]').getAttribute('content'));
