28731dfaed7b9b2cabd38b87b438b3bd3cc07c1f
Jun <bjyoon513@gmail.com>
2017-02-02 14:14:02 -0500

    make contentbox un-deletable

--- cite.js
+++ cite.js
@@ -6,11 +6,27 @@
 //chrome.runtime.sendMessage("author");
 
 
-var title = document.title;
-chrome.runtime.sendMessage(
-    {
-        "author": author,
-        "title": title,
-
+function getMetaProp(key, filler) {
+    var metas = document.getElementsByTagName('meta');
+    for (i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute("property") == key) {
+            return metas[i].getAttribute("content");
+        }
     }
-);
+    return filler; // return filler if none is found
+}
+
+var author = 'n.a',
+    date = 'n.d',
+    title = document.title,
+    siteName = "",
+    URL = window.location.href;
+
+author = document.querySelector('meta[name="author]').getAttribute('content');
+
+chrome.runtime.sendMessage({
+    "author": author,
+    "title": title,
+    "URL": URL
+  });
+  
--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 function e(id) {
     return document.getElementById(id)
 }
-
 var citeData;
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         citeData = request;
         // render citation information
-        e('title').innerHTML = citeData.title;
+        e('title').innerHTML = citeData.title,
         e('author').innerHTML = citeData.author;
-      }
+    }
 );
+
+chrome.tabs.executeScript(null, {file: "getcite.js"});
