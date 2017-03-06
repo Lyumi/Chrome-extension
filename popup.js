29d8cc648005b2b9ec3083571968c47cf2efe17e
Jun <bjyoon513@gmail.com>
2017-03-06 14:24:22 -0500

    replace textarea to div Bold text

--- popup.js
+++ popup.js
@@ -1,23 +1,23 @@
-
+var title= "Title: ";
 
 // When the popup HTML has loaded
 window.addEventListener('load', function(evt) {
     // Get the event page
     chrome.runtime.getBackgroundPage(function(eventPage) {
-        // Call the getPageInfo function in the event page, passing in 
-        // our onPageDetailsReceived function as the callback. This injects 
+        // Call the getPageInfo function in the event page, passing in
+        // our onPageDetailsReceived function as the callback. This injects
         // content.js into the current tab's HTML
         eventPage.getPageDetails(onPageDetailsReceived);
     });
 });
 
-// This callback function is called when the content script has been 
+// This callback function is called when the content script has been
 // injected and returned its results
-function onPageDetailsReceived(pageDetails)  { 
-    document.getElementById('title2').value = pageDetails.title2; 
+function onPageDetailsReceived(pageDetails)  {
+    document.getElementById('title').innerHTML = title.bold()+pageDetails.title2;
     console.log("Title: " + title2);
-    //document.getElementById('url').value = pageDetails.url; 
-    //document.getElementById('summary').innerText = pageDetails.summary; 
+    //document.getElementById('url').value = pageDetails.url;
+    //document.getElementById('summary').innerText = pageDetails.summary;
 }
 
 console.log("Hi from Popup");
