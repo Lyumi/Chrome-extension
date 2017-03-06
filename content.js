880592b1ca5588aec1b51b61e922d7058ae61977
josh <jcallsen6@gmail.com>
2017-03-06 13:41:02 -0500

    update, working title

--- content.js
+++ content.js
@@ -1,34 +1,8 @@
-//var author = 'n.a',
-//  date = 'n.d',
-//title = document.title,
-// siteName = "",
-// URL = window.location.href;
 
 
-
-function e(id) {
-    return document.getElementById(id)
-}
-
-
-
-
-//chrome.runtime.sendMessage(
-//  document.querySelector('meta[property="og:title"]').getAttribute('content')
-//);
-//chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerText);
-//chrome.runtime.sendMessage(document.querySelector('meta[property="og:title"]').getAttribute('content'));
-
-//chrome.runtime.sendMessage("author");
-
-var title = document.title;
-//var author = document.querySelector('meta[name="author]').getAttribute('content');
-console.log(title);
-//console.log(author);
-e('title').innerHTML=title;
-
-
-
-chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-  console.log(response.farewell);
+chrome.runtime.sendMessage({
+    'title2': document.title
+ 
 });
+
+console.log("Hi from Content");
--- popup.js
+++ popup.js
@@ -1,26 +1,23 @@
-//chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerText);
-//chrome.runtime.sendMessage(document.querySelector('meta[property="og:title"]').getAttribute('content'));
-//var title = document.querySelector('meta[property="og:title"]').getAttribute('content');
-//var author = document.querySelector('meta[name="author]').getAttribute('content');
 
-//chrome.runtime.sendMessage("author");
-//e('title').innerHTML = "test";
-console.log("123");
-chrome.runtime.sendMessage("hello", function(response) {
-    console.log("123");
-    console.log(response);
-  e('title').innerHTML = memes;
+
+// When the popup HTML has loaded
+window.addEventListener('load', function(evt) {
+    // Get the event page
+    chrome.runtime.getBackgroundPage(function(eventPage) {
+        // Call the getPageInfo function in the event page, passing in 
+        // our onPageDetailsReceived function as the callback. This injects 
+        // content.js into the current tab's HTML
+        eventPage.getPageDetails(onPageDetailsReceived);
+    });
 });
 
-
-//function e(id) {
-  //  return document.getElementById(id)
+// This callback function is called when the content script has been 
+// injected and returned its results
+function onPageDetailsReceived(pageDetails)  { 
+    document.getElementById('title2').value = pageDetails.title2; 
+    console.log("Title: " + title2);
+    //document.getElementById('url').value = pageDetails.url; 
+    //document.getElementById('summary').innerText = pageDetails.summary; 
 }
-var citeData;
 
-
-
-
-
-
-
+console.log("Hi from Popup");
