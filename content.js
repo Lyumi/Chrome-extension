9d2f8a56010a42ace4cb9b65c89f53c4fcabb7b1
Jun <bjyoon513@gmail.com>
2017-03-08 14:33:49 -0500

    get author, put everything into one citation working on Website title (ex. CNN)

--- content.js
+++ content.js
@@ -1,24 +1,18 @@
 var m_names = new Array("Jan", "Feb", "Mar",
-"Apr", "May", "Jun", "Jul", "Aug", "Sep",
-"Oct", "Nov", "Dec");
+    "Apr", "May", "Jun", "Jul", "Aug", "Sep",
+    "Oct", "Nov", "Dec");
 var d = new Date();
 var curr_date = d.getDate();
 var curr_month = d.getMonth();
 var curr_year = d.getFullYear();
 var author2 = 'n.a',
-    date2 = 'n.d',
+    published_date = 'n.d',
     title2 = document.title,
     websitetitle2 = window.location.hostname,
-<<<<<<< HEAD
     URL2 = window.location.href,
-    metas = document.getElementsByTagName('meta');
-=======
-    URL2 = window.location.href;
     publisher2 = 'N.P',
-    date3 = (curr_date + "-" + m_names[curr_month] + "-" + curr_year),
-    completecitationMLA = author2 + ". " +  '"' + title2 + '"' + ". " +  websitetitle2.italics() + ". " + publisher2 + ". " +  date2 + ". " +  "Web." +  date3 + ".";
->>>>>>> 9efc47878b9c28749c5d09acc5546cfd6cdc6547
-
+    accessed_date = (curr_date + "-" + m_names[curr_month] + "-" + curr_year);
+var metas = document.getElementsByTagName('meta');
 
 function getMetaName(key, filler) {
     for (var i = 0; i < metas.length; i++) {
@@ -40,22 +34,19 @@ function getMetaProp(key, filler) {
 }
 
 author2 = getMetaName('author', author2);
-console.log(author2);
+title2 = getMetaName('og:site_name', title2);
 
+var completecitationMLA = author2 + ". " + '"' + title2 + '"' + ". " + websitetitle2.italics() + ". " + publisher2 + ". " + published_date + ". " + "Web." + accessed_date + ".";
 chrome.runtime.sendMessage({
     'title': title2,
     'URL': URL2,
-<<<<<<< HEAD
-    'author' : author2,
-    //'date' : date2,
+    'author': author2,
+    'date': published_date,
     //'websitetitle' : websitetitle2,
     //'publisher' : publisher
-=======
-    //'author' = author2,
-    //'date' = date2,
     //'websitetitle' = websitetitle2,
     //'publisher' = publisher
     'citation': completecitationMLA
->>>>>>> 9efc47878b9c28749c5d09acc5546cfd6cdc6547
+
 
 });
