26175bcb3e7687e01f769d510589a771d46cdd46
Jun <bjyoon513@gmail.com>
2017-03-14 14:03:03 -0400

    fix conflict issue

--- content.js
+++ content.js
@@ -56,37 +56,15 @@ published_date = getMetaProp('og:pubdate', published_date);
 
 
 var completecitationMLA = author2 + ". " + '"' + title2 + '"' + ". " + websitetitle2.italics() + ". " + publisher2 + ". " + published_date + ". " + "Web." + accessed_dateMLA + ".";
-<<<<<<< HEAD
-var completecitationAPA = authorL + ", " + authorF + "." + accessed_date + ". " + title2 + ". " + "Retrieved from " + URL2;
-=======
-completecitationAPA = authorL +", " + authorF + "." +  accessed_date + ". " + title2 + ". " + "Retrieved from "+ URL2;
 
-//if(checked){
->>>>>>> 7203b162b3e1032dd6c7f448fcfea68876f10071
+var completecitationAPA = authorL +", " + authorF + "." +  accessed_date + ". " + title2 + ". " + "Retrieved from "+ URL2;
+
 chrome.runtime.sendMessage({
     'title'       : title2,
     'URL'         : URL2,
     'author'      : author2,
     'date'        : published_date,
     'websitetitle': websitetitle2,
-<<<<<<< HEAD
-    'citation'    : completecitationMLA
-=======
     'citation': completecitationMLA,
     'citation2': completecitationAPA
->>>>>>> 7203b162b3e1032dd6c7f448fcfea68876f10071
-
-
 });
-//}
-/*else{
-chrome.runtime.sendMessage({
-    'title': title2,
-    'URL': URL2,
-    'author': author2,
-    'date': published_date,
-    'websitetitle': websitetitle2,
-    'citation': completecitationAPA
-}
-
-*/
--- popup.js
+++ popup.js
@@ -1,15 +1,16 @@
-var title         = "Title: ",
-    author        = "Author: ",
-    websitetitle  = "Website: ",
+var title = "Title: ",
+    author = "Author: ",
+    websitetitle = "Website: ",
     datepublished = "Date Published: ",
-    publisher     ="Publisher: ",
-    URL           ="URL: ",
-    citation      = "Citation: ";
+    publisher = "Publisher: ",
+    URL = "URL: ",
+    citation = "Citation: ";
 
 
 function e(id) {
 
-   return document.getElementById(id) }
+    return document.getElementById(id)
+}
 
 
 // When the popup HTML has loaded
@@ -26,28 +27,19 @@ var checked = e('slider round').innerHTML.checked;
 // This callback function is called when the content script has been
 // injected and returned its results
 
-function onPageDetailsReceived(pageDetails)  {
-<<<<<<< HEAD
-    e('title').innerHTML             = title.bold()+pageDetails.title;
-    e('author').innerHTML            = author.bold()+pageDetails.author;
-    e('website title').innerHTML     = websitetitle.bold()+pageDetails.websitetitle;
-    e('date').innerHTML              = datepublished.bold()+pageDetails.date;
-    e('publisher').innerHTML         = publisher.bold()+pageDetails.publisher;
-    e('URL').innerHTML               = URL.bold()+pageDetails.URL;
-=======
+function onPageDetailsReceived(pageDetails) {
 
-    e('title').innerHTML = title.bold()+pageDetails.title;
-    e('author').innerHTML = author.bold()+pageDetails.author;
-    e('website title').innerHTML = websitetitle.bold()+pageDetails.websitetitle;
-    e('date').innerHTML = datepublished.bold()+pageDetails.date;
-    e('publisher').innerHTML = publisher.bold()+pageDetails.publisher;
-    e('URL').innerHTML = URL.bold()+pageDetails.URL;
-     if(checked){
->>>>>>> 7203b162b3e1032dd6c7f448fcfea68876f10071
-    e('complete citation').innerHTML =citation.bold() + pageDetails.citation;
-    }
-    else{
-        e('complete citation').innerHTML =citation.bold() + pageDetails.citation2;
+
+    e('title').innerHTML = title.bold() + pageDetails.title;
+    e('author').innerHTML = author.bold() + pageDetails.author;
+    e('website title').innerHTML = websitetitle.bold() + pageDetails.websitetitle;
+    e('date').innerHTML = datepublished.bold() + pageDetails.date;
+    e('publisher').innerHTML = publisher.bold() + pageDetails.publisher;
+    e('URL').innerHTML = URL.bold() + pageDetails.URL;
+    if (checked) {
+        e('complete citation').innerHTML = citation.bold() + pageDetails.citation;
+    } else {
+        e('complete citation').innerHTML = citation.bold() + pageDetails.citation2;
     }
 }
 
