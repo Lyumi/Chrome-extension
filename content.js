eff8ab43104ddc5ffc28d67ea2266d9e8d196bb0
Jun <bjyoon513@gmail.com>
2017-04-04 14:11:32 -0400

    Get the author from every kinds of site
    Finally did it haha...
    
    need to reprase the author

--- content.js
+++ content.js
@@ -25,6 +25,59 @@ var m_names = new Array("Jan", "Feb", "Mar",
     metas = document.getElementsByTagName('meta');
 
 
+function includes(str, array) {
+
+    // checks if any words from given array occurs in a string str
+    inv = false;
+    sctr = str.toLowerCase();
+
+    array.forEach(function(key) {
+        if (sctr.indexOf(key) >= 0) {
+            inv = true;
+        }
+    });
+
+    return inv;
+}
+
+function textNorm(string) {
+    // "normalizing" text for comparison purposes later
+    return string.trim().toLowerCase();
+}
+
+function universalAuthorParser(qx, log) {
+    current = author2;
+    // given a list of DOM elements, sets AUTHOR as the most appropriate choice found within the list
+    // also formats the AUTHOR variable value as comma-separate list string for multiple authors
+    if ($(qx).length >= 1 || author2 == "n.a") {
+
+        author2 = "";
+        var authors = $(qx);
+
+        counter = authors.length;
+        if (counter > 10) counter = 10;
+
+        for (var i = 0; i < counter; i++) {
+            // is the value of the element's content formatted correctly (not Twitter handles, user IDs, etc)?
+            if (author2.indexOf(textNorm(authors[i].innerText)) == -1 && !includes(authors[i].innerText, ['about ', 'author', '@', ':', '0'])) {
+                autin = textNorm(authors[i].innerText).split(' ');
+
+                for (var j = 0, jx = autin.length; j < jx; j++) {
+                    if (includes(autin[j], ['by', 'from', 'follow', 'share', '+'])) { // removes frivolous wording
+                        autin[j] = '';
+                    }
+                }
+                author2 += autin.join(' ');
+                if (i < authors.length - 1) { author2 += ", " }
+            }
+        }
+    }
+    if (author2 == "") {
+        author2 = current;
+    }
+}
+
+
 
 function getMetaName(key, filler) {
     for (var i = 0; i < metas.length; i++) {
@@ -46,23 +99,14 @@ function getMetaProp(key, filler) {
 }
 
 function getItemProp(key, filler) {
-    var spans= document.getElementsByTagName('span')
-    for (var i=0; i< length; i++){
-         if (spans[i].getAttribute('itemprop')== key){
+    var spans = document.getElementsByTagName('span')
+    for (var i = 0; i < length; i++) {
+        if (spans[i].getAttribute('itemprop') == key) {
             return spans[i].getAttribute('itemprop');
-         }
+        }
     }
     return filler;
 }
-
-function textNorm(string) {
-    // "normalizing" text for comparison purposes later
-    return string.trim().toLowerCase();
-}
-
-
-
-
 
 var author2 = 'n.a',
     published_date = 'n.d',
@@ -78,11 +122,31 @@ var author2 = 'n.a',
 
 websitetitle2 = getMetaProp('og:site_name', websitetitle2);
 
-author2 = getMetaName('author', author2);
-author2 =  getItemProp('name', author2);
-
 
 published_date = getMetaProp('og:pubdate', published_date);
+
+// getting authors
+// done in order of reliability
+universalAuthorParser("a[href*='journalist']");
+universalAuthorParser("a[href*='contributor']");
+universalAuthorParser("a[href*='author']");
+universalAuthorParser("[rel='author']");
+universalAuthorParser(".author");
+author2 = author2.split(",")[0]; // because above queries often picks up wrong authors, too
+universalAuthorParser(".byline");
+universalAuthorParser("[itemprop='author']");
+universalAuthorParser("[name*='DC.Creator']");
+universalAuthorParser("[name*='DC.creator']"); // for "creator" or "Creator" which both occur often as a result of the Dublin Core standard
+universalAuthorParser("head [property*=Author]");
+universalAuthorParser("head [property*=author]");
+if (getMetaProp("article:author", "") != "" && isNaN(parseInt(getMetaProp("article:author", "") == true)) && includes(getMetaProp("article:author"), ['facebook', 'author', 'subscribe', 'http']) == false) {
+    author2 = getMetaProp("article:author", "");
+}
+author2 = getMetaName('authors', getMetaName('author', author2));
+author2 = getMetaName('Author', author2);
+if (getMetaName("citation_author", "") != "") {
+    author2 = getMetaName("citation_author");
+}
 
 
 var completecitationMLA = author2 + ". " + '"' + title2 + '"' + ". " + websitetitle2.italics() + ". " + publisher2 + ". " + published_date + ". " + "Web." + accessed_dateMLA + ".";
