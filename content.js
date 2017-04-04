d3539e3bb29dbf0c9da03de967817f7c01972299
Jun <bjyoon513@gmail.com>
2017-04-04 13:27:48 -0400

    <a herf

--- content.js
+++ content.js
@@ -4,18 +4,25 @@ function e(id) {
 
 }
 
+var $ = function(selector) {
+    var nodes = document.querySelectorAll(selector),
+        a = [],
+        l = nodes.length < 24 ? nodes.length : 24;
+    for (i = 0; i < l; a[i] = nodes[i++]);
+    return a;
+}
 
 
 var m_names = new Array("Jan", "Feb", "Mar",
         "Apr", "May", "Jun", "Jul", "Aug", "Sep",
         "Oct", "Nov", "Dec"),
-    d          = new Date(),
-    curr_date  = d.getDate(),
+    d = new Date(),
+    curr_date = d.getDate(),
     curr_month = d.getMonth(),
-    curr_year  = d.getFullYear(),   
-    accessed_date    = (curr_date + " " + m_names[curr_month] + " " + curr_year),
-    accessed_dateMLA = (curr_date + " " + m_names[curr_month] + ", " + curr_year),    
-    metas            = document.getElementsByTagName('meta');
+    curr_year = d.getFullYear(),
+    accessed_date = (curr_date + " " + m_names[curr_month] + " " + curr_year),
+    accessed_dateMLA = (curr_date + " " + m_names[curr_month] + ", " + curr_year),
+    metas = document.getElementsByTagName('meta');
 
 
 
@@ -38,32 +45,55 @@ function getMetaProp(key, filler) {
     return filler;
 }
 
+function getItemProp(key, filler) {
+    var spans= document.getElementsByTagName('span')
+    for (var i=0; i< length; i++){
+         if (spans[i].getAttribute('itemprop')== key){
+            return spans[i].getAttribute('itemprop');
+         }
+    }
+    return filler;
+}
 
-var author2        = 'n.a',
+function textNorm(string) {
+    // "normalizing" text for comparison purposes later
+    return string.trim().toLowerCase();
+}
+
+
+
+
+
+var author2 = 'n.a',
     published_date = 'n.d',
-    title2         = document.title,
-    websitetitle2  = 'n.a',
-    URL2           = window.location.href,
-    publisher2 = getMetaName('copyright',"N.P"),
+    title2 = document.title,
+    websitetitle2 = 'n.a',
+    URL2 = window.location.href,
+    publisher2 = getMetaName('copyright', "N.P"),
     whitespaceChar = author2.indexOf(" ") >= 0,
-    authorL        = author2.substr(whitespaceChar),
-    authorF        = author2.substr(0, whitespaceChar);
+    authorL = author2.substr(whitespaceChar),
+    authorF = author2.substr(0, whitespaceChar);
 
 
-author2        = getMetaName('author', author2);
-websitetitle2  = getMetaProp('og:site_name', websitetitle2);
+
+websitetitle2 = getMetaProp('og:site_name', websitetitle2);
+
+author2 = getMetaName('author', author2);
+author2 =  getItemProp('name', author2);
+
+
 published_date = getMetaProp('og:pubdate', published_date);
 
 
 var completecitationMLA = author2 + ". " + '"' + title2 + '"' + ". " + websitetitle2.italics() + ". " + publisher2 + ". " + published_date + ". " + "Web." + accessed_dateMLA + ".";
 
-var completecitationAPA = authorL +", " + authorF + "." +  accessed_date + ". " + title2 + ". " + "Retrieved from "+ URL2;
+var completecitationAPA = authorL + ", " + authorF + "." + accessed_date + ". " + title2 + ". " + "Retrieved from " + URL2;
 
 chrome.runtime.sendMessage({
-    'title'       : title2,
-    'URL'         : URL2,
-    'author'      : author2,
-    'date'        : published_date,
+    'title': title2,
+    'URL': URL2,
+    'author': author2,
+    'date': published_date,
     'websitetitle': websitetitle2,
     'citationMLA': completecitationMLA,
     'citationAPA': completecitationAPA
